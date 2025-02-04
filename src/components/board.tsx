6f406be2606695ba29e6529c26d5d2af4120da67
Youngjoon Park <pyj2025@gmail.com>
2025-02-03 21:23:01 -0800

    remove unused codes

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -5,9 +5,6 @@ import useTaskStore, { Task, TaskList } from '@/lib/store/task-store';
 
 const Board: React.FC = () => {
   const { tasks, moveTask } = useTaskStore();
-  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
-  const [taskContent, setTaskContent] = React.useState('');
-  const [editingTask, setEditingTask] = React.useState<Task | null>(null);
 
   const handleDragStart = (
     e: React.DragEvent<HTMLDivElement>,
@@ -33,12 +30,6 @@ const Board: React.FC = () => {
     if (sourceList === targetList) return;
 
     moveTask(taskId, sourceList, targetList);
-  };
-
-  const handleAddTaskButton = () => {
-    setEditingTask(null);
-    setTaskContent('');
-    setIsDialogOpen(true);
   };
 
   return (
